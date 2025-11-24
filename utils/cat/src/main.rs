19e20ac515e18f919b8c14e817123736bf8023aa
Morten Munk <mortenmunk97@gmail.com>
2025-11-24 20:18:29 +0100

    cat

--- utils/cat/src/main.rs
+++ utils/cat/src/main.rs
@@ -1,5 +1,25 @@
-use std::env::args;
+use std::{
+    env::args,
+    fs::File,
+    io::{BufReader, Read, Result, Write, stdout},
+};
 
-fn main() {
-    let args: Vec<String> = args().collect();
+fn main() -> Result<()> {
+    let args: Vec<String> = args().skip(1).collect();
+    if !args.is_empty() {
+        for x in args {
+            let mut reader = BufReader::new(File::open(&x)?);
+            let stdout = stdout();
+            let mut handle = stdout.lock();
+            let mut buffer = [0_u8; 8192];
+            loop {
+                let n = reader.read(&mut buffer)?;
+                if n == 0 {
+                    break;
+                }
+                handle.write_all(&buffer[..n])?;
+            }
+        }
+    }
+    Ok(())
 }
